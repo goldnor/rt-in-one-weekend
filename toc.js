3dbcc7400c653358301dda5f06efafd59d32c2dd
Timo Sonnenschein <timo@coconana-studios.com>
2025-07-10 19:52:56 +0200

    First draft

---
+++ toc.js
@@ -0,0 +1,70 @@
+// Populate the sidebar
+//
+// This is a script, and not included directly in the page, to control the total size of the book.
+// The TOC contains an entry for each page, so if each page includes a copy of the TOC,
+// the total size of the page becomes O(n**2).
+class MDBookSidebarScrollbox extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = '<ol class="chapter"><li class="chapter-item expanded affix "><a href="chapters/disclaimer.html">Disclaimer</a></li><li class="chapter-item expanded affix "><a href="chapters/cover.html">Cover</a></li><li class="chapter-item expanded "><a href="chapters/overview.html"><strong aria-hidden="true">1.</strong> Overview</a></li><li class="chapter-item expanded "><div><strong aria-hidden="true">2.</strong> Output an Image</div></li><li><ol class="section"><li class="chapter-item expanded "><a href="chapters/output_an_image/the_ppm_image_format.html"><strong aria-hidden="true">2.1.</strong> The PPM Image Format</a></li><li class="chapter-item expanded "><a href="chapters/output_an_image/creating_an_image_file.html"><strong aria-hidden="true">2.2.</strong> Creating an Image File</a></li><li class="chapter-item expanded "><a href="chapters/output_an_image/adding_a_progress_indicator.html"><strong aria-hidden="true">2.3.</strong> Adding a Progress Indicator</a></li></ol></li><li class="chapter-item expanded "><a href="chapters/the_vec3_class/the_vec3_class.html"><strong aria-hidden="true">3.</strong> The vec3 class</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="chapters/the_vec3_class/color_utility_functions.html"><strong aria-hidden="true">3.1.</strong> Color Utility Functions</a></li></ol></li><li class="chapter-item expanded "><div><strong aria-hidden="true">4.</strong> Rays, a Simple Camera, and Background</div></li><li><ol class="section"><li class="chapter-item expanded "><a href="chapters/rays_a_simple_camera_and_background/the_ray_class.html"><strong aria-hidden="true">4.1.</strong> The ray class</a></li><li class="chapter-item expanded "><a href="chapters/rays_a_simple_camera_and_background/sending_rays_into_the_scene.html"><strong aria-hidden="true">4.2.</strong> Sending Rays Into the Scene</a></li></ol></li><li class="chapter-item expanded "><a href="chapters/adding_a_sphere/adding_a_sphere.html"><strong aria-hidden="true">5.</strong> Adding a Sphere</a></li><li><ol class="section"><li class="chapter-item expanded "><a href="chapters/adding_a_sphere/ray_sphere_intersection.html"><strong aria-hidden="true">5.1.</strong> Ray-Sphere Intersection</a></li><li class="chapter-item expanded "><a href="chapters/adding_a_sphere/creating_our_first_raytraced_image.html"><strong aria-hidden="true">5.2.</strong> Creating Our First Raytraced Image</a></li></ol></li></ol>';
+        // Set the current, active page, and reveal it if it's hidden
+        let current_page = document.location.href.toString().split("#")[0].split("?")[0];
+        if (current_page.endsWith("/")) {
+            current_page += "index.html";
+        }
+        var links = Array.prototype.slice.call(this.querySelectorAll("a"));
+        var l = links.length;
+        for (var i = 0; i < l; ++i) {
+            var link = links[i];
+            var href = link.getAttribute("href");
+            if (href && !href.startsWith("#") && !/^(?:[a-z+]+:)?\/\//.test(href)) {
+                link.href = path_to_root + href;
+            }
+            // The "index" page is supposed to alias the first chapter in the book.
+            if (link.href === current_page || (i === 0 && path_to_root === "" && current_page.endsWith("/index.html"))) {
+                link.classList.add("active");
+                var parent = link.parentElement;
+                if (parent && parent.classList.contains("chapter-item")) {
+                    parent.classList.add("expanded");
+                }
+                while (parent) {
+                    if (parent.tagName === "LI" && parent.previousElementSibling) {
+                        if (parent.previousElementSibling.classList.contains("chapter-item")) {
+                            parent.previousElementSibling.classList.add("expanded");
+                        }
+                    }
+                    parent = parent.parentElement;
+                }
+            }
+        }
+        // Track and set sidebar scroll position
+        this.addEventListener('click', function(e) {
+            if (e.target.tagName === 'A') {
+                sessionStorage.setItem('sidebar-scroll', this.scrollTop);
+            }
+        }, { passive: true });
+        var sidebarScrollTop = sessionStorage.getItem('sidebar-scroll');
+        sessionStorage.removeItem('sidebar-scroll');
+        if (sidebarScrollTop) {
+            // preserve sidebar scroll position when navigating via links within sidebar
+            this.scrollTop = sidebarScrollTop;
+        } else {
+            // scroll sidebar to current active section when navigating via "next/previous chapter" buttons
+            var activeSection = document.querySelector('#sidebar .active');
+            if (activeSection) {
+                activeSection.scrollIntoView({ block: 'center' });
+            }
+        }
+        // Toggle buttons
+        var sidebarAnchorToggles = document.querySelectorAll('#sidebar a.toggle');
+        function toggleSection(ev) {
+            ev.currentTarget.parentElement.classList.toggle('expanded');
+        }
+        Array.from(sidebarAnchorToggles).forEach(function (el) {
+            el.addEventListener('click', toggleSection);
+        });
+    }
+}
+window.customElements.define("mdbook-sidebar-scrollbox", MDBookSidebarScrollbox);
